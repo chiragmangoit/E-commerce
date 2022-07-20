1356bcfe544f2e48e39e0804b8a896211fced71b
Chirag Bajaj <chirag.mangoit@gmail.com>
2022-07-20 16:04:45 +0530

    range-slider

--- src/app/e-commerce/shared/shared.module.ts
+++ src/app/e-commerce/shared/shared.module.ts
@@ -14,6 +14,7 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductNavigateComponent } from './components/product-navigate/product-navigate.component';
 import { CartComponent } from './components/cart/cart.component';
+import { NouisliderModule } from 'ng2-nouislider';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { CartComponent } from './components/cart/cart.component';
     HeaderRoutingModule,
     CarouselModule,
     BrowserAnimationsModule,
+    NouisliderModule
   ],
   exports: [
     HeaderComponent,
