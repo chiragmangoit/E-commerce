cfcc6a273183ce2c669cf289eee945361f1d5a37
Chirag Bajaj <chirag.mangoit@gmail.com>
2022-07-22 16:15:27 +0530

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './e-commerce/core/pages/home/home.component';
-import { ProductDetailsComponent } from './e-commerce/features/product/pages/product-details/product-details.component';
-import { ProductListComponent } from './e-commerce/features/product/pages/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'product', component: ProductListComponent },
-  { path: 'product-detail', component: ProductDetailsComponent },
+
 ];
 
 @NgModule({
