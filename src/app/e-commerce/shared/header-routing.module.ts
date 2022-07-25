b5e96a536806b4fc1cfb210340201333766c54b7
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-07-25 11:14:00 +0530

    dszfgvd

--- src/app/e-commerce/shared/header-routing.module.ts
+++ src/app/e-commerce/shared/header-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginRegisterComponent } from '../features/auth/pages/login-register/login-register.component';
+import { WhishListComponent } from '../features/whish-list/whish-list.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginRegisterComponent },
+  { path: 'wish-list', component: WhishListComponent }
 ];
 
 @NgModule({
