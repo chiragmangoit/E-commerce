f7e4fd6e54dde98823ec1462991c66dc40acb72b
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-07-25 15:46:21 +0530

    Product nav

--- src/app/e-commerce/features/product/components/product-detail-nav/product-detail-nav.component.ts
+++ src/app/e-commerce/features/product/components/product-detail-nav/product-detail-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product-detail-nav',
@@ -9,12 +11,45 @@ export class ProductDetailNavComponent implements OnInit {
   constructor() {}
 
   stars: number[] = [1, 2, 3, 4, 5];
-  selectedValue: number;
+  selectedValue: number = 0;
+  isMouseover = true;
 
-  countStar(star) {
+  productData = [
+    {
+      id: 1,
+      product_name: "Easy Polo Black Edition",
+      banner: "http://95.111.202.157/mangoproject/public/images/1658323272.product4.jpg",
+      price: "55",
+      Web_ID: "awdesaw",
+      Availability: "adaewd",
+      Condition: "dqaswdea",
+      Brand: "sfdsf",
+      details: "sdff",
+    }
+  ]
+
+  countStar(star: number) {
+    this.isMouseover = false;
     this.selectedValue = star;
-    console.log('Value of star', star);
   }
+
+  addClass(star: number) {
+    if (this.isMouseover) {
+      this.selectedValue = star;
+    }
+  }
+
+  removeClass() {
+    if (this.isMouseover) {
+      this.selectedValue = 0;
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    console.log(this.selectedValue);
+    
+  }  
 
   ngOnInit(): void {}
 }
