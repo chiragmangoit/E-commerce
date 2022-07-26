0ae8ae6f1885cf941e2c94ba6fafdebf5c1fb2c8
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-07-26 11:26:50 +0530

    wish service

--- src/app/e-commerce/data/services/wish-list.service.ts
+++ src/app/e-commerce/data/services/wish-list.service.ts
@@ -3,27 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WishListService {
+  constructor() {}
 
-  constructor() { }
-  
-  wishListServiceData:Product[] = [];
+  wishListServiceData: Product[] = [];
 
-  addWishListData(data:Product) {
-
-  if (this.wishListServiceData.includes(data)) {
-    return
-  }
-
+  addWishListData(data: Product) {
+    if (this.wishListServiceData.find((item) => item['id'] === data['id'])) {
+      alert('The product alredy exist in wish list');
+      return;
+    }
     this.wishListServiceData.push(data);
-    
+    alert('The product is added to wish list');
   }
-    
+
   getWishListData() {
     return this.wishListServiceData;
   }
 
-
+  removeProduct(i) {
+    this.wishListServiceData.splice(i, 1);
+  }
 }
