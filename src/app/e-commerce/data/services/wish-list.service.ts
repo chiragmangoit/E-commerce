ddb438d526d5dcb20a08afa6aa98a49f36036432
Chirag Bajaj <chirag.mangoit@gmail.com>
2022-07-26 15:09:10 +0530

    wishlist

--- src/app/e-commerce/data/services/wish-list.service.ts
+++ src/app/e-commerce/data/services/wish-list.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../models/product.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishListService {
-  constructor() {}
+  constructor(private http: HttpClient, private route: Router) {}
 
   wishListServiceData: Product[] = [];
 
   addWishListData(data: Product) {
-    if (this.wishListServiceData.find((item) => item['id'] === data['id'])) {
-      alert('The product alredy exist in wish list');
-      return;
+    if (localStorage.length != 0) {
+      if (this.wishListServiceData.find((item) => item['id'] === data['id'])) {
+        alert('The product alredy exist in wish list');
+        return;
+      }
+      this.wishListServiceData.push(data);
+      alert('The product is added to wish list');
+    } else {
+      this.route.navigate(['/login']);
     }
-    this.wishListServiceData.push(data);
-    alert('The product is added to wish list');
   }
 
   getWishListData() {
