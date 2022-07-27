4ce15b1e29d8f7bd18e88157ebbc25953f6e865d
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-07-27 12:37:57 +0530

    wish list

--- src/app/e-commerce/features/whish-list/wish-list.module.ts
+++ src/app/e-commerce/features/whish-list/wish-list.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WhishListComponent } from './whish-list.component';
-
-@NgModule({
-  declarations: [WhishListComponent],
-  imports: [CommonModule],
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from '../../core/guards/auth-guard.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptorService } from '../../core/interceptors/auth-interceptor.service';
 
-const routes: Routes = [{ path: 'wish-list', component: WhishListComponent,canActivate:[AuthGuardService] }];
+const routes: Routes = [
+  {
+    path: 'wish-list',
+    component: WhishListComponent,
+    canActivate: [AuthGuardService],
+  },
+];
 
 @NgModule({
   declarations: [WhishListComponent],
