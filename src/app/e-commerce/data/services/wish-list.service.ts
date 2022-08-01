de62cb0984ce098e8cecc53bbe850d3ab913b7c7
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-08-01 10:18:18 +0530

    Wish list service

--- src/app/e-commerce/data/services/wish-list.service.ts
+++ src/app/e-commerce/data/services/wish-list.service.ts
@@ -3,39 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../models/product.model';
 import { wishList } from '../models/wish-list.model';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WishListService {
-  constructor(private http: HttpClient) {}
-
   constructor(private http: HttpClient, private route: Router) {}
+
+  wishListEmit = new Subject<any>();
 
   wishListServiceData: 'http://95.111.202.157/mangoproject/public/api/wishlist';
 
   addWishListData(data: Product) {
-    // if (this.daata.find((item) => item['id'] === data['id'])) {
-    //   alert('The product alredy exist in wish list');
-    //   return;
-    // }
-       
     let userData = JSON.parse(localStorage.getItem('userData'));
     let url = 'http://95.111.202.157/mangoproject/public/api/wishlist';
     return this.http.post<wishList>(url, {
       user_id: userData.userId,
-      product_id: data['id']
+      product_id: data['id'],
     });
-    if (localStorage.length != 0) {
-      if (this.wishListServiceData.find((item) => item['id'] === data['id'])) {
-        alert('The product alredy exist in wish list');
-        return;
-      }
-      this.wishListServiceData.push(data);
-      alert('The product is added to wish list');
-    } else {
-      this.route.navigate(['/login']);
-    }
   }
 
   getWishListData() {
@@ -45,13 +31,8 @@ export class WishListService {
   }
 
   removeProduct(id) {
-    let url = 'http://95.111.202.157/mangoproject/public/api/wishlist-remove/' + id;
+    let url =
+      'http://95.111.202.157/mangoproject/public/api/wishlist-remove/' + id;
     return this.http.get<wishList>(url);
   }
 }
-
-// getProductDetails(id: number) {
-//   let url =
-//     'http://95.111.202.157/mangoproject/public/api/product-details/' + id;
-//   return this.http.get<ProductDetails>(url);
-// }
